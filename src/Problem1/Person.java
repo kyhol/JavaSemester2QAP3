51a87e16366718aaf0d1600839997d7d074489fa
kyhol <Kyle.Hollett@keyin.com>
2024-11-17 12:42:16 -0330

    Started Person parent class. getters/setters to string.

--- src/Problem1/Person.java
+++ src/Problem1/Person.java
@@ -1,4 +1,45 @@
 package Problem1;
 
 public class Person {
+    protected String myName;
+    protected int myAge;
+    protected String myGender;
+
+    public Person(String name, int age, String gender) {
+        myName = name;
+        myAge = age;
+        myGender = gender;
+    }
+
+    // Getter methods
+    public String getName() {
+        return myName;
+    }
+
+    public int getAge() {
+        return myAge;
+    }
+
+    public String getGender() {
+        return myGender;
+    }
+
+    // Setter methods
+    public void setName(String name) {
+        this.myName = name;
+    }
+
+    public void setAge(int age) {
+        this.myAge = age;
+    }
+
+    public void setGender(String gender) {
+        this.myGender = gender;
+    }
+
+    // tostring method
+    @Override
+    public String toString() {
+        return myName + ", age: " + myAge + ", gender: " + myGender;
+    }
 }
