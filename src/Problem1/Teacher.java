2f10fdff8c8505756b5d00c3db4728770462cdfa
kyhol <Kyle.Hollett@keyin.com>
2024-11-17 13:12:47 -0330

    Teacher extends Person Private instead of protected, as the variables only apply to teacher comments added in Person class - forgot to mention in class commit

--- src/Problem1/Teacher.java
+++ src/Problem1/Teacher.java
@@ -1,4 +1,37 @@
 package Problem1;
 
-public class Teacher {
+public class Teacher extends Person {
+    //private variables to teacher
+    private String subject;
+    private double salary;
+
+    //super used to call from person constructor
+    public Teacher(String name, int age, String gender, String subject, double salary) {
+        super(name, age, gender);
+        this.subject = subject;
+        this.salary = salary;
+    }
+
+    // Getter methods
+    public String getSubject() {
+        return subject;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    // Setter methods
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", subject: " + subject + ", salary: $" + salary;
+    }
 }
