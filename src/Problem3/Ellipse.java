06c23cc98e97000f0c8d269453900cd2105b94f7
kyhol <Kyle.Hollett@keyin.com>
2024-11-19 12:14:56 -0330

    updated ellipse and shape

--- src/Problem3/Ellipse.java
+++ src/Problem3/Ellipse.java
@@ -1,4 +1,23 @@
 package Problem3;
 
-public class Ellipse {
+public class Ellipse extends Shape {
+    protected double a;  // major
+    protected double b;  // minor
+
+    public Ellipse(String name, double axis1, double axis2) {
+        super(name);
+        // make sure a is large than b
+        this.a = Math.max(axis1, axis2);
+        this.b = Math.min(axis1, axis2);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return Math.PI * (2 * Math.sqrt((a * a + b * b) / 2));
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * a * b;
+    }
 }
--- src/Problem3/Shape.java
+++ src/Problem3/Shape.java
@@ -7,4 +7,12 @@ public abstract class Shape {
     public Shape(String name) {
         this.name = name;
     }
+    public abstract double getPerimeter();
+    public abstract double getArea();
+
+    @Override
+    public String toString() {
+        return String.format("%s - Area: %.2f, Perimeter: %.2f",
+                name, getArea(), getPerimeter());
+    }
 }
