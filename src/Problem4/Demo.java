276c546f4a58873deddc1373ba6641875429f3fa
kyhol <Kyle.Hollett@keyin.com>
2024-11-21 08:45:17 -0330

    Added scaling logic answered questions screenshoot of output added cleaned up println for problem 4 Before/After scaling implemented Overrides added for scaling Maybe there's more I just woke up and did this last night :D

--- src/Problem4/Demo.java
+++ src/Problem4/Demo.java
@@ -1,34 +1,55 @@
 package Problem4;
 
-import Problem3.Circle;
-import Problem3.Ellipse;
-import Problem3.EquilateralTriangle;
-import Problem3.Shape;
-import Problem3.Triangle;
-
 public class Demo {
-    public static void main(String[] args) {
+    // Add static method for scaling shapes
+    public static void scaleShapes(Scalable[] shapes, double factor) {
+        System.out.println("\n\t\t\tScaling Shapes by factor: " + factor);
+        System.out.println("-------------------------------------------------------");
 
-        Problem3.Shape[] shapes = {
+        System.out.println("Before scaling:");
+        for (Scalable s : shapes) {
+            System.out.println(s);
+        }
+
+        // Applied scaling
+        for (Scalable s : shapes) {
+            s.scale(factor);
+        }
+
+        System.out.println("\nAfter scaling:");
+        for (Scalable s : shapes) {
+            System.out.println(s);
+        }
+        System.out.println("-------------------------------------------------------");
+    }
+
+    public static void main(String[] args) {
+        Shape[] shapes = {
                 new Circle("Circle A", 11.0),
                 new Ellipse("Ellipse A", 10.0, 6.0),
-                new Problem3.Triangle("Triangle A", 4.0, 5.0, 6.0),
+                new Triangle("Triangle A", 4.0, 5.0, 6.0),
                 new EquilateralTriangle("Equilateral Triangle A", 9.0)
         };
+
         System.out.println("\n");
-        System.out.println("\t\t\t\t  Shape Information:");
-        System.out.println("------------------------------------------------------");
+        System.out.println("\t\t\t\t  Initial Shape Information:");
+        System.out.println("-------------------------------------------------------");
         for (Shape shape : shapes) {
             System.out.println(shape);
         }
-        System.out.println("------------------------------------------------------");
+        System.out.println("-------------------------------------------------------");
 
-        try {
-            System.out.println("\nTrying to create invalid triangle...");
-            Problem3.Triangle invalidTriangle = new Triangle("Invalid", 1.0, 1.0, 10.0);
-        } catch (Exception e) {
-            System.out.println("Failed to create invalid triangle");
+//        // Calling / Testing scale with different factors
+        scaleShapes(shapes, 2.0);  // double the size
+        scaleShapes(shapes, 0.5);  // half the size
 
-        }
+//        // Test invalid triangle creation
+//        try {
+//            System.out.println("\nTrying to create invalid triangle...");
+//            Triangle invalidTriangle = new Triangle("Invalid", 1.0, 1.0, 10.0);
+//        } catch (Exception e) {
+//            System.out.println("Failed to create triangle");
+//            System.out.println("-------------------------------------------------------");
+//        }
     }
 }
